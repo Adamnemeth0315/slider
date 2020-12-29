1f82c7852033228104a6e9edcc5b53a8a27aaec9
Adam <adam.nemeth.0315@gmail.com>
2020-12-29 16:05:55 +0100

    Már a nyilaknál a kép címét is kiírja de a pontoknál még nem működik

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const container = document.querySelector('.body__image--container');
 const arrowLeft = document.querySelector('.slider__button--left');
 const arrowRight = document.querySelector('.slider__button--right');
 const dots = document.querySelectorAll('.fa-circle');
+const tittle = document.querySelector('.tittle');
+const images = document.querySelectorAll('img');
 let currentSlide = 0;
 const slideSize = 800;
 
@@ -18,16 +20,36 @@ const slidePictureFunction = () => {
 const nextPictureFUnction = () => {
     currentSlide = currentSlide >= slides -1 ? 0 : currentSlide + 1;
     slidePictureFunction();
+    showTittle();
 }
 
 const previousPictureFUnction = () => {
     currentSlide = currentSlide <= 0 ? slides -1 : currentSlide -1;
     slidePictureFunction();
+    showTittle();
 }
 
 const imageSelect = () => dots.forEach((element, index) => element.addEventListener('click', () => container.style.transform = `translateX(-${index * slideSize}px)`));
 
 
+
+
+//Ez így nem oké még. 
+  const showTittle = () => {
+    let images = {
+        0 : 'Trees',
+        1 : 'Lake',
+        2 : 'Jungle',
+        3 : 'Bubble',
+        4 : 'North',
+    };
+
+    tittle.textContent = images[currentSlide];
+};  
+
+
+
 imageSelect();
 arrowLeft.addEventListener('click', previousPictureFUnction);
-arrowRight.addEventListener('click', nextPictureFUnction);
+arrowRight.addEventListener('click', nextPictureFUnction); 
+
